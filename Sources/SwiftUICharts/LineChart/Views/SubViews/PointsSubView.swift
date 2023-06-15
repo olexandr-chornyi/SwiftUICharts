5b3efb0600eca45efd6b07388b2e4b4945a20301
Oleksandr Chornyi <oleksandr.chornyi@sephora.com>
2023-06-15 21:30:28 +0200

    add some fixes

--- Sources/SwiftUICharts/LineChart/Views/SubViews/PointsSubView.swift
+++ Sources/SwiftUICharts/LineChart/Views/SubViews/PointsSubView.swift
@@ -73,15 +73,14 @@ internal struct PointsSubView<DS>: View where DS: CTLineChartDataSet,
             }
         case .outline:
             ForEach(dataSets.dataPoints.indices, id: \.self) { index in
-                VStack {
-                    Point(value: dataSets.dataPoints[index].value,
-                          index: index,
-                          minValue: minValue,
-                          range: range,
-                          datapointCount: dataSets.dataPoints.count,
-                          pointSize: dataSets.pointStyle.pointSize,
-                          ignoreZero: dataSets.style.ignoreZero,
-                          pointStyle: dataSets.pointStyle.pointShape)
+                Point(value: dataSets.dataPoints[index].value,
+                       index: index,
+                       minValue: minValue,
+                       range: range,
+                       datapointCount: dataSets.dataPoints.count,
+                       pointSize: dataSets.pointStyle.pointSize,
+                       ignoreZero: dataSets.style.ignoreZero,
+                       pointStyle: dataSets.pointStyle.pointShape)
                     .ifElse(!isFilled, if: {
                         $0.trim(to: animationValue)
                             .stroke(dataSets.dataPoints[index].pointColour?.border ?? dataSets.pointStyle.borderColour,
@@ -91,12 +90,23 @@ internal struct PointsSubView<DS>: View where DS: CTLineChartDataSet,
                             .stroke(dataSets.dataPoints[index].pointColour?.border ?? dataSets.pointStyle.borderColour,
                                     lineWidth: dataSets.pointStyle.lineWidth)
                     })
-                    if dataSets.pointStyle.showValue {
-                        Text("\(dataSets.dataPoints[index].value)")
-                            .font(Font.system(size: 7.0))
-                            .foregroundColor(dataSets.pointStyle.fillColour)
-                            .frame(alignment: .center)
-                    }
+//                if dataSets.pointStyle.showValue {
+//
+////                    let point: CGRect = CGRect(x: pointX, y: pointY, width: dataSets.pointStyle.pointSize, height: pointSize)
+//
+//
+//                    Text("\(dataSets.dataPoints[index].value)")
+//                        .font(Font.system(size: 7.0))
+//                        .foregroundColor(dataSets.pointStyle.fillColour)
+//                        .position(x: pointX, y: pointY)
+//                }
+                GeometryReader { geometry in
+                            let frame = geometry.frame(in: CoordinateSpace.local)
+                    Text("0")
+                        .font(Font.system(size: 7.0))
+                        .foregroundColor(dataSets.pointStyle.fillColour)
+                        .position(x: frame.origin.x, y: frame.origin.y)
+     //                       return Text("\(frame.origin.x), \(frame.origin.y), \(frame.size.width), \(frame.size.height)")
                 }
             }
             .animateOnAppear(disabled: disableAnimation, using: animation) {
@@ -105,6 +115,21 @@ internal struct PointsSubView<DS>: View where DS: CTLineChartDataSet,
             .animateOnDisappear(disabled: disableAnimation, using: animation) {
                 self.startAnimation = false
             }
+//            .overlay(
+//                        GeometryReader { proxy in
+//                            let x: CGFloat = dataSets.pointStyle.pointSize.width / CGFloat(dataSets.dataPoints.count-1)
+//                            let y: CGFloat = dataSets.pointStyle.pointSize.height / CGFloat(range)
+//                            let offset: CGFloat = dataSets.pointStyle.pointSize / CGFloat(2)
+//
+//                            let pointX: CGFloat = (CGFloat(index) * x) - offset
+//                            let pointY: CGFloat = ((CGFloat(dataSets.dataPoints[index].value - minValue) * -y) + dataSets.pointStyle.pointSize.height) - offset
+//
+//                            Text("\(dataSets.dataPoints[index].value)")
+//                                .font(Font.system(size: 7.0))
+//                                .foregroundColor(dataSets.pointStyle.fillColour)
+//                                .position(x: 10, y: 20)
+//                        }
+//                    )
         case .filledOutLine:
             ForEach(dataSets.dataPoints.indices, id: \.self) { index in
                 Point(value: dataSets.dataPoints[index].value,
